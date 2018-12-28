dd2dae6d3184355fa5c3ba92f6445bd2d3a7e021
cgmsuccess <cgmsuccess@163.com>
2018-12-28 15:40:07 +0800

    作者：陈桂民 修改:  测试 111

---
+++ Quartz2D/Quartz2D/cxxtest1111view.h
@@ -0,0 +1,17 @@
+//
+//  cxxtest1111view.h
+//  Quartz2D
+//
+//  Created by 链派社区 on 2018/12/28.
+//  Copyright © 2018 cgm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface cxxtest1111view : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
