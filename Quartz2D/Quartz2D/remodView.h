8234c5f9e9507386678ec3951230a319ba75576c
cgmsuccess <cgmsuccess@163.com>
2018-12-28 15:33:57 +0800

    作者：陈桂民 修改: redemeView

---
+++ Quartz2D/Quartz2D/remodView.h
@@ -0,0 +1,17 @@
+//
+//  remodView.h
+//  Quartz2D
+//
+//  Created by 链派社区 on 2018/12/28.
+//  Copyright © 2018 cgm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface remodView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quartz2D/Quartz2D/remodView.m
@@ -0,0 +1,21 @@
+//
+//  remodView.m
+//  Quartz2D
+//
+//  Created by 链派社区 on 2018/12/28.
+//  Copyright © 2018 cgm. All rights reserved.
+//
+
+#import "remodView.h"
+
+@implementation remodView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
